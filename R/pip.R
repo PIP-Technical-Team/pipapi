3adf860bbb0a54cebad287c35adf099fa122505b
Tony Fujs <tfujs@worldbank.org>
2021-04-14 14:11:44 +0200

    add basic handling of regional computations

--- R/pip.R
+++ R/pip.R
@@ -4,6 +4,7 @@ pip <- function(country   = "all",
                 year      = "all",
                 fill_gaps = FALSE,
                 aggregate = FALSE,
+                group_by  = NULL,
                 welfare_type = "all",
                 svy_coverage = "all",
                 ppp       = NULL,
@@ -55,6 +56,58 @@ pip <- function(country   = "all",
       out <- dplyr::bind_rows(out, aggregated)
     }
   }
+
+
+  # Handle grouped aggregations
+  if (!is.null(group_by)) {
+    pop_rgn <- lkups[["pop_region"]]
+    # Handle potential (insignificant) difference in poverty_line values  that
+    # may mess-up the grouping
+    out$poverty_line <- povline
+
+    # out2 <- dplyr::filter(out,
+    #                       is_used_for_aggregation == TRUE)
+    out2 <- dplyr::select(out,
+                          pcn_region_code,
+                          reporting_year,
+                          poverty_line,
+                          # mean,
+                          headcount,
+                          poverty_gap,
+                          poverty_severity,
+                          watts,
+                          reporting_pop)
+
+    # Compute group aggregates
+    out2 <- dplyr::group_by(out2, pcn_region_code, reporting_year, poverty_line)
+    out2 <- dplyr::summarise(out2,
+                             # mean              = weighted.mean(x = mean, w = reporting_pop, na.rm = TRUE),
+                             headcount         = weighted.mean(x = headcount, w = reporting_pop, na.rm = TRUE),
+                             poverty_gap       = weighted.mean(x = poverty_gap, w = reporting_pop, na.rm = TRUE),
+                             poverty_severity  = weighted.mean(x = poverty_severity, w = reporting_pop, na.rm = TRUE),
+                             watts             = weighted.mean(x = watts, w = reporting_pop, na.rm = TRUE))
+
+    # Compute world aggregates
+    out3 <- dplyr::left_join(out2, pop_rgn)
+
+    out3 <- dplyr::group_by(out3,
+                            reporting_year, poverty_line)
+
+    out3 <- dplyr::summarise(out3,
+                             # mean              = weighted.mean(x = mean, w = pop, na.rm = TRUE),
+                             headcount         = weighted.mean(x = headcount, w = pop, na.rm = TRUE),
+                             poverty_gap       = weighted.mean(x = poverty_gap, w = pop, na.rm = TRUE),
+                             poverty_severity  = weighted.mean(x = poverty_severity, w = pop, na.rm = TRUE),
+                             watts             = weighted.mean(x = watts, w = pop, na.rm = TRUE))
+
+    out3$pcn_region_code <- "WLD"
+
+    # Combine
+    out <- dplyr::bind_rows(out2, out3)
+
+    return(out)
+  }
+
 
   # Add pre-computed distributional statistics
   dist_stats <- dplyr::select(lkup$dist_stats,
