45bcd115ce7363dfa2f5d4d87b6f1621ff3bc4e2
Tony Fujs <tfujs@worldbank.org>
2021-07-20 11:55:19 +0200

    fix package dependencies

--- R/plot.R
+++ R/plot.R
@@ -1,10 +1,10 @@
-custom_plot <- function(lkups) {
-
-  p <- ggplot2::ggplot(lkups$svy_lkup, ggplot2::aes(x = reporting_year,
-                                                    y = survey_mean_ppp,
-                                                    color = pcn_region_code,
-                                                    group = pcn_region_code)) +
-    ggplot2::geom_smooth()
-
-  plotly::ggplotly(p)
-}
+# custom_plot <- function(lkups) {
+#
+#   p <- ggplot2::ggplot(lkups$svy_lkup, ggplot2::aes(x = reporting_year,
+#                                                     y = survey_mean_ppp,
+#                                                     color = pcn_region_code,
+#                                                     group = pcn_region_code)) +
+#     ggplot2::geom_smooth()
+#
+#   plotly::ggplotly(p)
+# }
