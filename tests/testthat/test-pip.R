df4b2c35889eef6305811e8e9aca346941eb1291
Tony Fujs <tfujs@worldbank.org>
2021-07-26 16:25:51 +0200

    add unit tests for core pip() function

--- tests/testthat/test-pip.R
+++ tests/testthat/test-pip.R
@@ -1,56 +1,129 @@
 library(dplyr)
 
-# main <- fst::read_fst("../../TEMP/data/DB/main.fst") %>%
-#   filter(distribution_type == "micro")
-# # pfw$surveyid_year <- as.character(pfw$surveyid_year)
-# svy_lkup <- fst::read_fst("../../TEMP/data/DB/dsm.fst") %>%
-#   filter(distribution_type == "micro")
-# ref_lkup <- fst::read_fst("../../TEMP/data/DB/refyear.fst") %>%
-#   filter(distribution_type == "micro")
-#
-# paths <- fs::dir_ls("../../TEMP/data/survey_data/", recurse = FALSE, type = "file")
-#
-# lkups <- list(svy_lkup = svy_lkup,
-#               ref_lkup = ref_lkup)
-#
-# test_that("output type is correct", {
-#
-#   tmp <- pip(country = "all",
-#              year    = "all",
-#              povline = 3.5,
-#              lkup = lkups,
-#              paths = paths)
-#
-#   expect_equal(class(tmp), "data.frame")
-# })
-#
-# test_that("welfare_type selection are correct", {
-#
-#   tmp <- pip(country = "all",
-#              year    = "all",
-#              povline = 3.5,
-#              lkup = svy_lkup,
-#              welfare_type = "all",
-#              paths = paths)
-#
-#   expect_equal(sort(unique(tmp$welfare_type)), c("consumption", "income"))
-#
-#   tmp <- pip(country = "all",
-#              year    = "all",
-#              povline = 3.5,
-#              lkup = svy_lkup,
-#              welfare_type = "consumption",
-#              paths = paths)
-#
-#   expect_equal(unique(tmp$welfare_type), "consumption")
-#
-#   tmp <- pip(country = "all",
-#              year    = "all",
-#              povline = 3.5,
-#              lkup = svy_lkup,
-#              welfare_type = "income",
-#              paths = paths)
-#
-#   expect_equal(unique(tmp$welfare_type), "income")
-# })
+# lkups <- pipapi:::clean_api_data(
+#   data_folder_root = "./tests/testdata/20210401/")
 
+
+lkups <- pipapi:::clean_api_data(
+  data_folder_root = "../testdata/20210401/")
+
+# Check output type ----
+test_that("output type is correct", {
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups)
+
+  expect_equal(class(tmp), c("data.table", "data.frame"))
+})
+
+# Check selections ----
+## Welfare type ----
+test_that("welfare_type selection are correct", {
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             welfare_type = "all")
+
+  expect_equal(sort(unique(tmp$welfare_type)), c("consumption", "income"))
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             welfare_type = "consumption")
+
+  expect_equal(unique(tmp$welfare_type), "consumption")
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             welfare_type = "income")
+
+  expect_equal(unique(tmp$welfare_type), "income")
+})
+## Reporting level ----
+test_that("reporting_level selection are correct", {
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             reporting_level = "all")
+
+  expect_equal(sort(unique(tmp$pop_data_level)), c("national", "rural", "urban"))
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             reporting_level = "national")
+
+  expect_equal(sort(unique(tmp$pop_data_level)), c("national"))
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             reporting_level = "rural")
+
+  expect_equal(sort(unique(tmp$pop_data_level)), c("rural"))
+
+  tmp <- pip(country = "all",
+             year    = "all",
+             povline = 3.5,
+             lkup = lkups,
+             reporting_level = "urban")
+
+  expect_equal(sort(unique(tmp$pop_data_level)), c("urban"))
+})
+
+# Check aggregation ----
+skip("Aggregation not correctly implemented")
+test_that("Aggregation is working", {
+tmp <- pip(country = "all",
+           year    = "all",
+           povline = 3.5,
+           aggregate = TRUE,
+           lkup = lkups)
+
+expect_equal(nrow(tmp), 1)
+})
+
+
+# Check imputation ----
+test_that("Imputation is working", {
+tmp <- pip(country = "all",
+           year    = "all",
+           povline = 3.5,
+           fill_gaps = TRUE,
+           lkup = lkups)
+
+expect_equal(nrow(tmp), 182)
+})
+
+
+# Check regional aggregations
+test_that("Regional aggregations are working", {
+tmp <- pip(country = "all",
+           year    = 2008,
+           group_by = "wb",
+           povline = 3.5,
+           lkup = lkups)
+
+expect_equal(nrow(tmp), 3)
+})
+
+# Check pop_share
+test_that("pop_share option is working", {
+  tmp <- pip(country = "AGO",
+             year    = 2000,
+             popshare = .2,
+             lkup = lkups)
+
+  expect_equal(nrow(tmp), 3)
+})
