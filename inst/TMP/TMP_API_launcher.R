461c50bbc9460d9ed4b44623cfa9b6e60642903b
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-08-05 03:53:06 -0400

    Minor changes

--- inst/TMP/TMP_API_launcher.R
+++ inst/TMP/TMP_API_launcher.R
@@ -1,22 +1,7 @@
 lkups <- pipapi:::clean_api_data(
-  data_folder_root = "C:/Users/wb499754/OneDrive - WBG/PIP/pip_data_master/ITSES-POVERTYSCORE-DATA/20210401/")
-
-
-
+  data_folder_root = Sys.getenv('DATA_FOLDER_ROOT'))
 
 start_api(api_version = "v1",
           port = 80,
           host = "0.0.0.0")
-
-
-# test_that("Plumber working directi", {
-#   # Send API request
-#   r <- httr::GET(root_path, port = 8000, path = "api/v1/get-root")
-#
-#   # Check response
-#   r$status_code
-#   httr::content(r, encoding = "UTF-8")
-#
-#   tmp <- function() {
-#     x <<- 3
-#   }
+          
