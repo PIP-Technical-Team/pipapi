252c49844fc97af300e65d958727fcbfa3002985
Tony Fujs <tfujs@worldbank.org>
2021-08-10 11:11:10 +0200

    change ENV variable name to avoid name collision

--- inst/TMP/TMP_API_launcher.R
+++ inst/TMP/TMP_API_launcher.R
@@ -1,7 +1,6 @@
 lkups <- pipapi:::clean_api_data(
-  data_folder_root = Sys.getenv('DATA_FOLDER_ROOT'))
+  data_folder_root = Sys.getenv('PIPAPI_DATA_FOLDER_ROOT'))
 
 start_api(api_version = "v1",
           port = 80,
           host = "0.0.0.0")
-          
