e8000b3943b83b8dc1ebde8979a44c79a025107a
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-09-10 10:29:35 -0400

    Minor fixes

--- inst/plumber/v1/plumber.R
+++ inst/plumber/v1/plumber.R
@@ -3,6 +3,7 @@ library(logger)
 library(glue)
 
 endpoints_path <- system.file("plumber/v1/endpoints.R", package = "pipapi")
+convert_empty <- pipapi::convert_empty
 
 plumber::pr(endpoints_path) %>%
   # pre-route log
