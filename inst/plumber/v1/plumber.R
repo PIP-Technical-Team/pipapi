3e4679a4b3b2dbc2f682522545d630bcdd80d9cf
Aleksander Eilertsen <aeilertsen@worldbank.org>
2021-09-13 07:52:07 -0400

    Minor adjustments
    Add log seperator and prefixes for route and serialize.

--- inst/plumber/v1/plumber.R
+++ inst/plumber/v1/plumber.R
@@ -8,12 +8,13 @@ convert_empty <- pipapi:::convert_empty
 plumber::pr(endpoints_path) %>%
   # pre-route log
   plumber::pr_hook("preroute", function() {
+    log_separator()
     tictoc::tic("route") # Start timer for log info
   }) %>%
   # post-route log
   plumber::pr_hook("postroute", function(req, res) {
     end_route <- tictoc::toc(quiet = TRUE)
-    log_info('{convert_empty(req$REMOTE_ADDR)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(req$QUERY_STRING)}  {convert_empty(res$status)} {round(end_route$toc - end_route$tic, digits = getOption("digits", 6))}')
+    log_info('route: {convert_empty(req$REMOTE_ADDR)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(req$QUERY_STRING)}  {convert_empty(res$status)} {round(end_route$toc - end_route$tic, digits = getOption("digits", 6))}')
   }) %>%
   # pre-serialization log
   plumber::pr_hook("preserialize", function() {
@@ -22,7 +23,9 @@ plumber::pr(endpoints_path) %>%
   # post-serialization log
   plumber::pr_hook("postserialize", function(req) {
     end_serial <- tictoc::toc(quiet = TRUE)
-    log_info('{convert_empty(req$PATH_INFO)} {round(end_serial$toc - end_serial$tic, digits = getOption("digits", 6))}') }) %>%
+    log_info('serialize: {convert_empty(req$PATH_INFO)} {round(end_serial$toc - end_serial$tic, digits = getOption("digits", 6))}')
+    # log_separator()
+  }) %>%
   plumber::pr_hook("exit", function() {
     log_info('Bye bye: {proc.time()[["elapsed"]]}')
   })
