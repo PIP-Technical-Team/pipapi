a7f0d41f00d1dedbda56587a85541b1b438a807e
Tony Fujs <tfujs@worldbank.org>
2021-09-17 11:27:07 +0200

    remove file read logs

--- inst/TMP/TMP_API_launcher.R
+++ inst/TMP/TMP_API_launcher.R
@@ -1,4 +1,5 @@
 lkups <- pipapi::create_lkups(Sys.getenv('PIPAPI_DATA_ROOT_FOLDER'))
+# lkups <- pipapi::create_versioned_lkups("C:/Users/wb499754/OneDrive - WBG/PIP/pip_data_master/ITSES-POVERTYSCORE-DATA/")
 
 start_api(api_version = "v1",
           port = 80,
