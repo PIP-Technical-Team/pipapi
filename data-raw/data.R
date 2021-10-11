e029dba595d8f75140c4e9c7f2bf36d96dc46730
Tony Fujs <tfujs@worldbank.org>
2021-10-11 16:54:10 +0200

    update empty response to mathc new column selection

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,8 +1,10 @@
-lkups <- pipapi::create_lkups(Sys.getenv('PIPAPI_DATA_ROOT_FOLDER'))
+lkups <- pipapi::create_versioned_lkups(Sys.getenv('PIPAPI_DATA_ROOT_FOLDER'))
+lkups <- lkups$versions_paths$latest_release
 empty_response <- pip('AGO', 2000, lkup = lkups)[-1]
 reporting_level_list <- c("national", "rural", "urban")
 
-usethis::use_data(empty_response,
+usethis::use_data(
+  empty_response,
   reporting_level_list,
   overwrite = TRUE
 )
