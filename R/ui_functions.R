4cb5ffad4c9c5caaca869610eef9abd3df444ad0
Tony Fujs <tony.fujs@gmail.com>
2022-01-24 23:17:03 -0300

    filter out 1990 for hp-stacked response

--- R/ui_functions.R
+++ R/ui_functions.R
@@ -11,7 +11,7 @@ ui_hp_stacked <- function(povline = 1.9,
                           lkup) {
 
   ref_years <- sort(unique(lkup$ref_lkup$reporting_year))
-  ref_years <- ref_years[!ref_years %in% c(1981:1989)]
+  ref_years <- ref_years[!ref_years %in% c(1981:1990)]
   ### TMP FIX START
   ### Ad hoc filtering of recent years
   ### To be removed once full censoring is correctly implemented
