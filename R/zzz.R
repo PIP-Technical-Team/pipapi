e4abf7972c2c5dda3dbb182d10eeefdcc156f824
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-02-15 08:14:30 -0500

    Toggle cache on/off with env var
    Add if statement to only activate caching when Sys.getenv("PIPAPI_CACHE") == "TRUE".

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
 .onLoad <- function(libname, pkgname) {
-  d <- rappdirs::user_cache_dir("pipapi")
-  log <- sprintf("%s/cache.log", d)
-  cd <- cachem::cache_disk(d, evict = "lru", logfile = log, max_size = 5 * 1024 * 1024^2) # 5GB
-  pip <<- memoise::memoise(pip, cache = cd)
-  assign("cd", cd, envir = .GlobalEnv)
+  if (Sys.getenv("PIPAPI_CACHE") == "TRUE") {
+    d <- rappdirs::user_cache_dir("pipapi")
+    log <- sprintf("%s/cache.log", d)
+    cd <- cachem::cache_disk(d, evict = "lru", logfile = log, max_size = 5 * 1024 * 1024^2) # 5GB
+    pip <<- memoise::memoise(pip, cache = cd)
+    assign("cd", cd, envir = .GlobalEnv)
+    message("Info: Disk based caching is enabled.")
+  }
 }
