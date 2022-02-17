bc3578e3b129762b1d72293beb1d659d898c4e86
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-02-17 05:49:01 -0500

    Use env var for max size limit

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   if (Sys.getenv("PIPAPI_APPLY_CACHING") == "TRUE") {
     d <- rappdirs::user_cache_dir("pipapi")
     log <- sprintf("%s/cache.log", d)
-    cd <- cachem::cache_disk(d, evict = "lru", logfile = log, max_size = 5 * 1024 * 1024^2) # 5GB
+    cd <- cachem::cache_disk(d, evict = "lru", logfile = log, max_size = as.numeric(Sys.getenv("PIPAPI_CACHE_MAX_SIZE")))
     pip <<- memoise::memoise(pip, cache = cd)
     assign("cd", cd, envir = .GlobalEnv)
     message("Info: Disk based caching is enabled.")
