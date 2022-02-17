431dda9ddbeeff6764a18afda599bd1e1dca7de3
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-02-17 05:40:06 -0500

    Rename env var

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  if (Sys.getenv("PIPAPI_CACHE") == "TRUE") {
+  if (Sys.getenv("PIPAPI_APPLY_CACHING") == "TRUE") {
     d <- rappdirs::user_cache_dir("pipapi")
     log <- sprintf("%s/cache.log", d)
     cd <- cachem::cache_disk(d, evict = "lru", logfile = log, max_size = 5 * 1024 * 1024^2) # 5GB
