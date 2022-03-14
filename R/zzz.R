909e19a72fc3e5caad98208a907e5aa2bc56cd75
Aeilert <aeilertsen@worldbank.org>
2022-03-14 08:29:51 -0400

    Add caching for pip_grp()

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,7 @@
     log <- sprintf("%s/cache.log", d)
     cd <- cachem::cache_disk(d, evict = "lru", logfile = log, max_size = as.numeric(Sys.getenv("PIPAPI_CACHE_MAX_SIZE")))
     pip <<- memoise::memoise(pip, cache = cd)
+    pip_grp <<- memoise::memoise(pip_grp, cache = cd)
     assign("cd", cd, envir = .GlobalEnv)
     message("Info: Disk based caching is enabled.")
   }
