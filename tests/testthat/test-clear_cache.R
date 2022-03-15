9d170c58c96237ea4ef538a52c461aa431dcdfa1
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-03-15 12:14:02 -0400

    Align with dev

--- tests/testthat/test-clear_cache.R
+++ tests/testthat/test-clear_cache.R
@@ -2,7 +2,7 @@
 skip_if(Sys.getenv("PIPAPI_DATA_ROOT_FOLDER") == "")
 
 lkups <- create_versioned_lkups(Sys.getenv("PIPAPI_DATA_ROOT_FOLDER"))
-lkups <- lkups$versions_paths$latest_release
+lkups <- lkups$versions_paths[[lkups$latest_release]
 
 # Tests depend on a specific temp cache directory. Skip if not found.
 cd <- readRDS('tests/testdata/cd-ex.RDS')
