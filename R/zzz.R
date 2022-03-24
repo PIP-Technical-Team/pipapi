c27c767e143ff8df62a83149f2157d3d06ab3f08
Aleksander Eilertsen <aeilertsen@worldbank.org>
2022-03-24 14:54:47 -0400

    Hot fix

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,14 @@
-.onLoad <- function(libname, pkgname) {
-  if (Sys.getenv("PIPAPI_APPLY_CACHING") == "TRUE") {
-    d <- rappdirs::user_cache_dir("pipapi")
-    log <- sprintf("%s/cache.log", d)
-    cd <- cachem::cache_disk(d,
-                             evict = "lru",
-                             logfile = log,
-                             max_size = as.numeric(Sys.getenv("PIPAPI_CACHE_MAX_SIZE")))
-    pip <<- memoise::memoise(pip, cache = cd)
-    pip_grp <<- memoise::memoise(pip_grp, cache = cd)
-    assign("cd", cd, envir = .GlobalEnv)
-    packageStartupMessage("Info: Disk based caching is enabled.")
-  }
-}
+# .onLoad <- function(libname, pkgname) {
+#   if (Sys.getenv("PIPAPI_APPLY_CACHING") == "TRUE") {
+#     d <- rappdirs::user_cache_dir("pipapi")
+#     log <- sprintf("%s/cache.log", d)
+#     cd <- cachem::cache_disk(d,
+#                              evict = "lru",
+#                              logfile = log,
+#                              max_size = as.numeric(Sys.getenv("PIPAPI_CACHE_MAX_SIZE")))
+#     pip <<- memoise::memoise(pip, cache = cd)
+#     pip_grp <<- memoise::memoise(pip_grp, cache = cd)
+#     assign("cd", cd, envir = .GlobalEnv)
+#     packageStartupMessage("Info: Disk based caching is enabled.")
+#   }
+# }
