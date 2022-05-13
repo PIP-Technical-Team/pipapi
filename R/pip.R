2b22ef1a7c7350ec59b06c203a137bb066823b7e
Ronak Shah <shahronak47@yahoo.in>
2022-05-13 16:22:33 +0530

    sort the rows

--- R/pip.R
+++ R/pip.R
@@ -148,6 +148,9 @@ pip <- function(country = "all",
                    "mean",
                    "pop_in_poverty")]
 
+    #Order rows by country code and reporting year
+    out <- out[with(out, order(country_code, reporting_year))]
+
     return(out)
   }
   # **** TO BE REMOVED **** REMOVAL ENDS HERE
