070c7b1c73f2c7cd9824cca49d36421388ea3439
Ronak Shah <shahronak47@yahoo.in>
2022-05-31 22:07:08 +0800

    include valid_years function in create_versioned_lkups function

--- R/valid_years.R
+++ R/valid_years.R
@@ -4,10 +4,6 @@
 #'
 #' @return numeric vector of valid years
 #' @export
-#' @examples
-#' lkups <- create_versioned_lkups(data_dir = "/pip-fake-data")
-#' lkup <- lkups$versions_paths$`20200101_2011_01_01_PROD`
-#' valid_years(lkup$data_root)
 
 valid_years <- function(data_dir) {
   tmp <- pipapi::get_aux_table(data_dir, 'interpolated_means')
