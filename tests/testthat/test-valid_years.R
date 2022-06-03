a5fe07ee742fb9864e46df095236e56f94148422
Tony Fujs <tony.fujs@gmail.com>
2022-06-03 17:32:20 +0200

    fix path to test data and update unit test to account for the new function returned value

--- tests/testthat/test-valid_years.R
+++ tests/testthat/test-valid_years.R
@@ -1,5 +1,17 @@
-test_that("valid_years correctly returns the valid years", {
-  expect_equal(valid_years('../pip-fake-data/20200101_2011_01_01_PROD/'), 1981:2019)
-  expect_equal(valid_years('../pip-fake-data/20211212_2011_01_01_PROD/'), 1981:2019)
+test_that("valid_years returns expected output type", {
+  tmp <- valid_years('../testdata/app_data/20210401/')
+  expect_equal(class(tmp), "list")
+  expect_equal(length(tmp), 2)
+  expect_equal(names(tmp), c("valid_survey_years", "valid_interpolated_years"))
+})
+
+test_that("valid_years correctly returns valid years", {
+  tmp <- valid_years('../testdata/app_data/20210401/')
+  # Survey years
+  expect_equal(min(tmp$valid_survey_years), 1967)
+  expect_equal(max(tmp$valid_survey_years), 2021)
+  expect_equal(length(tmp$valid_survey_years), 50)
+  # Interpolated years
+  expect_equal(tmp$valid_interpolated_years, 1981:2019)
 })
 
