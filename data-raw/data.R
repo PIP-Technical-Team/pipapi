c4c2b6b9c4b3c5d5ba4a15001d963dbd41f8b316
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-08-12 17:26:13 -0400

    update data/ folder to make it work...

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,14 +1,19 @@
-lkups <- pipapi::create_versioned_lkups(Sys.getenv('PIPAPI_DATA_ROOT_FOLDER'))
-lkups <- lkups$versions_paths[[lkups$latest_release]]
-empty_response <- pip('AGO', 2000, lkup = lkups)[-1]
+library(pipapi)
+# lkups <- pipapi::create_versioned_lkups(Sys.getenv('PIPAPI_DATA_ROOT_FOLDER'))
+lkups <- create_versioned_lkups(Sys.getenv('PIPAPI_FAKEDATA_FOLDER'))
+lkups <- lkups$versions_paths$`20211212_2011_01_01_PROD`
+
+ctr <- "NGA"
+
+empty_response <- pip(ctr, 2012, lkup = lkups)[-1]
 reporting_level_list <- c("national", "rural", "urban")
 
-tmp <- ui_cp_charts(country = "AGO", povline = 1.9, lkup = lkups)
-tmp1 <- tmp$AGO$pov_charts[[1]]$pov_trend[-c(1:3)]
-tmp2 <- tmp$AGO$pov_charts[[1]]$pov_mrv[-c(1:11)]
+tmp <- ui_cp_charts(country = ctr, povline = 1.9, lkup = lkups)
+tmp1 <- tmp[[ctr]]$pov_charts[[1]]$pov_trend[-c(1:3)]
+tmp2 <- tmp[[ctr]]$pov_charts[[1]]$pov_mrv[-c(1:11)]
 empty_response_cp_poverty <- list(pov_trend = tmp1, pov_mrv = tmp2)
 
-empty_response_grp <- pip_grp('AGO', 2000, lkup = lkups)[-1]
+empty_response_grp <-  pip(ctr, 2012, lkup = lkups)[-1]
 
 usethis::use_data(
   empty_response,
@@ -17,3 +22,4 @@ usethis::use_data(
   empty_response_grp,
   overwrite = TRUE
 )
+
