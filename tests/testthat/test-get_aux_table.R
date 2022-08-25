6064acf004fb2a597bff6837d0a576e4f0d72edb
Tony Fujs <tony.fujs@gmail.com>
2022-08-25 12:32:06 +0200

    remove one unit test no longer needed

--- tests/testthat/test-get_aux_table.R
+++ tests/testthat/test-get_aux_table.R
@@ -17,7 +17,4 @@ test_that("get_aux_table() works", {
 test_that("get_aux_table() returns an error", {
   expect_error(pipapi::get_aux_table(data_folder_root, "../survey_data/ARG_1980_EPH_D2_INC_GROUP.fst"),
                "Error opening fst file for reading, please check access rights and file availability")
-
-  expect_error(pipapi::get_aux_table(data_folder_root, "decomposition", long_format = TRUE),
-               "Please select one of cpi, ppp, gdp, pce and pop tables to get data in long format.")
 })
