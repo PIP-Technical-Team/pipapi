10bf965d43c65a6d5a5e95aa8d3c00557058fbe8
Tony Fujs <tony.fujs@gmail.com>
2023-02-14 10:42:58 +0100

    Remove cache logging and add dedicated endpoints instead + increase prune rate

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,18 @@
 .onLoad <- function(libname, pkgname) {
   if (Sys.getenv("PIPAPI_APPLY_CACHING") == "TRUE") {
     d <- rappdirs::user_cache_dir("pipapi")
-    log <- sprintf("%s/cache.log", d)
+    # log <- sprintf("%s/cache.log", d)
     cd <- cachem::cache_disk(d,
                              evict = "lru",
-                             logfile = log,
-                             max_size = as.numeric(Sys.getenv("PIPAPI_CACHE_MAX_SIZE")))
+                             logfile = NULL,
+                             max_size = as.numeric(Sys.getenv("PIPAPI_CACHE_MAX_SIZE")),
+                             prune_rate = 50)
     pip <<- memoise::memoise(pip, cache = cd, omit_args = "lkup")
     pip_grp <<- memoise::memoise(pip_grp, cache = cd, omit_args = "lkup")
     pip_grp_logic <<- memoise::memoise(pip_grp_logic, cache = cd, omit_args = "lkup")
     ui_cp_charts <<- memoise::memoise(ui_cp_charts, cache = cd, omit_args = "lkup")
     ui_cp_download <<- memoise::memoise(ui_cp_download, cache = cd, omit_args = "lkup")
-    # ui_cp_key_indicators <<- memoise::memoise(ui_cp_key_indicators, cache = cd)
-    # ui_cp_charts <<- memoise::memoise(ui_cp_charts, cache = cd)
+
     assign("cd", cd, envir = .GlobalEnv)
     packageStartupMessage("Info: Disk based caching is enabled.")
   }
