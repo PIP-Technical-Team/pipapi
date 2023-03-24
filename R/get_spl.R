190b1576807a339e3c4cd8c8b7b704c4cefd0808
Tony Fujs <tony.fujs@gmail.com>
2023-03-24 18:48:16 +0100

    first iteration of get_spl() function

---
+++ R/get_spl.R
@@ -0,0 +1,20 @@
+#' Return Societal Poverty Line (SPL) statistics
+#'
+#' @param country character: Country ISO 3 codes
+#' @param data_dir character: Data directory
+#'
+#' @return data.frame
+#' @export
+
+get_spl <- function(country = "ALL",
+                    data_dir) {
+  out <- get_aux_table(data_dir = data_dir,
+                       table = "spl",
+                       long_format = FALSE)
+
+  if (!"ALL" %chin% country) {
+    out <- out[out[["country_code"]] %chin% country, ]
+  }
+
+  return(out)
+}
