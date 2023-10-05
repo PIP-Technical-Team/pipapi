a6611d18c7bf36c04b1304028c2ca547b8782b6d
tonyfujs <tony.fujs@gmail.com>
2023-10-05 18:07:27 +0200

    further refactoring

--- R/create_countries_vctr.R
+++ R/create_countries_vctr.R
@@ -22,7 +22,6 @@ create_countries_vctr <- function(country,
       user_off_reg      = NULL, # Off regs requested by user
       user_alt_agg      = NULL, # Alt aggs requested by user
       est_ctrs          = NULL, # All countries needed for estimations
-      fg_ctrs           = NULL, # All countries (with svy) needed for estimations when fill_gaps = TRUE
       md_off_reg        = NULL, # off regs needed to input to missing data
       md_year           = NULL, # years to input to missing data countries
       grp_use           = NULL, # Instruction for the next function call
@@ -84,90 +83,22 @@ create_countries_vctr <- function(country,
   user_alt_gt_code <- get_user_x_code(user_alt_gt)
   ctr_alt_agg      <- get_ctr_alt_agg(user_alt_gt      = user_alt_gt,
                                       user_alt_gt_code = user_alt_gt_code,
+                                      user_alt_agg = user_alt_agg,
                                       cl               = aux_files$country_list )
   ## Implicit SURVEY countries in both, official and alternative ----
   impl_ctrs <- get_impl_ctrs(user_gt = user_gt,
                              user_gt_code = user_gt_code,
+                             user_aggs = user_aggs,
                              ctrs = ctrs)
   ## All countries needed for estimations -----
   est_ctrs <- unique(c(user_ctrs, impl_ctrs))
-
-
-  # Early Return ---------
-  # if (off_alt_agg == "off") {
-  #
-  #   return(lret)
-  # }
-
-
   ## Countries with  missing data ----
-  md <- filter_md(md = aux_files$missing_data,
-                  ctr_alt_agg = ctr_alt_agg,
-                  year = year)
-
-  # Get countries for which we want to input
-  yes_md <- nrow(md) > 0
-  if (yes_md) {
-    md_off_reg <- unique(md[["region_code"]])
-    md_year    <- unique(md[["year"]])
-
-    if (off_alt_agg == "both") {
-      # filter md_off_reg and md_year based on what have already been
-      # estimated
-
-      grp_computed <-
-        expand.grid(region_code      = user_off_reg,
-                    reporting_year   = as.numeric(year), # Hot fix
-                    stringsAsFactors = FALSE) |>
-        data.table::as.data.table()
-
-      grp_to_compute <-
-        expand.grid(region_code      = md_off_reg,
-                    reporting_year   = md_year,
-                    stringsAsFactors = FALSE)  |>
-        data.table::as.data.table()
-
-      grp_to_compute <-
-        grp_to_compute[!grp_computed,
-                       on = c("region_code", "reporting_year")]
-
-      # filter region code and year to calculate
-      md_off_reg <- unique(grp_to_compute[["region_code"]])
-      md_year    <- unique(grp_to_compute[["year"]])
-
-      if (length(md_off_reg) > 0) {
-        # If length of `md_off_reg` is still positive, we need to append the
-        # results previously calculated
-        grp_use <- "append"
-      } else {
-        # If length of `md_off_reg` is zero, we don't need to do any additional
-        # calculation, so the previously done calculation is used alone
-        grp_use <- "alone"
-      }
-
-    } else {
-      # If there is no previous calculation, there is nothing to use from
-      # before.
-      grp_use <- "not"
-    }
-
-    md_ctrs <- unique(md[["country_code"]]) # missing data countries
-    # fg_ctrs <- ctr_alt_agg[!ctr_alt_agg %in% md_ctrs] # survey countries
-
-  } else { # if yes_md == FALSE
-    md_ctrs    <- character() # missing data countries
-    # fg_ctrs    <- ctr_alt_agg  # survey countries
-    md_off_reg <- character()
-    md_year    <- numeric()
-    grp_use    <- character()
-    md         <- character()
-  }
-
-  fg_ctrs <- est_ctrs # survey countries
-
-  #   _____________________________________________________________________
-  #   Return                                                           ####
-
+  md_vars <- get_md_vars(md           = md,
+                         ctr_alt_agg  = ctr_alt_agg,
+                         year         = year,
+                         off_alt_agg  = off_alt_agg,
+                         user_off_reg = user_off_reg)
+  list2env(md_vars, envir = environment())
   # Add to return list
   fillin_list(lret)
 
@@ -279,6 +210,7 @@ get_user_x_code <- function(x) {
 #'
 #' @param user_alt_gt character: Grouping type needed by user
 #' @param user_alt_gt_code character: Grouping type code
+#' @param user_alt_agg character: Alternate aggregates requested by user
 #' @param cl data.frame: Countries lookup table
 #'
 #' @return character
@@ -286,6 +218,7 @@ get_user_x_code <- function(x) {
 #'
 get_ctr_alt_agg <- function(user_alt_gt,
                             user_alt_gt_code,
+                            user_alt_agg,
                             cl) {
   if (!is_empty(user_alt_gt)) {
 
@@ -307,6 +240,7 @@ get_ctr_alt_agg <- function(user_alt_gt,
 #'
 #' @param user_gt character: Grouping type
 #' @param user_gt_code character: Grouping type code
+#' @param user_aggs character: Aggregates selected by user
 #' @param ctrs data.frame: Countries lookup table
 #'
 #' @return character
@@ -314,6 +248,7 @@ get_ctr_alt_agg <- function(user_alt_gt,
 #'
 get_impl_ctrs <- function(user_gt,
                           user_gt_code,
+                          user_aggs,
                           ctrs) {
   if (!is_empty(user_gt)) {
     out   <- user_gt_code |>
@@ -328,6 +263,111 @@ get_impl_ctrs <- function(user_gt,
     out <- character()
   }
   return(out)
+}
+
+#' Helper function to retrieve the country/year pairs to be computed
+#'
+#' @param user_off_reg character: Official regions requested by user
+#' @param md_off_reg character: Missing data for official regions
+#' @param year character: Years
+#' @param md_year character: Years with missing data
+#'
+#' @return data.frame
+#' @export
+#'
+get_grp_to_compute <- function(user_off_reg,
+                               md_off_reg,
+                               year,
+                               md_year) {
+  grp_computed <-
+    expand.grid(region_code      = user_off_reg,
+                reporting_year   = as.numeric(year), # Hot fix
+                stringsAsFactors = FALSE) |>
+    data.table::as.data.table()
+
+  grp_to_compute <-
+    expand.grid(region_code      = md_off_reg,
+                reporting_year   = md_year,
+                stringsAsFactors = FALSE)  |>
+    data.table::as.data.table()
+
+  grp_to_compute <-
+    grp_to_compute[!grp_computed,
+                   on = c("region_code", "reporting_year")]
+
+  return(grp_to_compute)
+}
+
+
+#' Helper function to retrieve variables needed to handle imputation of
+#' missing data
+#'
+#' @param md data.frame: Table of country/year with missing data
+#' @param ctr_alt_agg character: Countries in alternative aggregate
+#' @param year character: Years
+#' @param off_alt_agg character: Instruction about how to handle official and
+#' alternate aggregates
+#' @param user_off_reg: character: Official regions requested by user
+#' @return list
+#' @export
+#'
+get_md_vars <- function(md,
+                        ctr_alt_agg,
+                        year,
+                        off_alt_agg,
+                        user_off_reg) {
+  md <- filter_md(md = md,
+                  ctr_alt_agg = ctr_alt_agg,
+                  year = year)
+  ## Get countries for which we want to impute
+  yes_md <- nrow(md) > 0
+  if (yes_md) {
+    md_off_reg <- unique(md[["region_code"]])
+    md_year    <- unique(md[["year"]])
+
+    if (off_alt_agg == "both") {
+      # filter md_off_reg and md_year based on what have already been
+      # estimated
+
+      grp_to_compute <- get_grp_to_compute(user_off_reg = user_off_reg,
+                                           md_off_reg   = md_off_reg,
+                                           year         = year,
+                                           md_year      = md_year)
+
+      # filter region code and year to calculate
+      md_off_reg <- unique(grp_to_compute[["region_code"]])
+      md_year    <- unique(grp_to_compute[["year"]])
+
+      if (length(md_off_reg) > 0) {
+        # If length of `md_off_reg` is still positive, we need to append the
+        # results previously calculated
+        grp_use <- "append"
+      } else {
+        # If length of `md_off_reg` is zero, we don't need to do any additional
+        # calculation, so the previously done calculation is used alone
+        grp_use <- "alone"
+      }
+
+    } else {
+      # If there is no previous calculation, there is nothing to use from
+      # before.
+      grp_use <- "not"
+    }
+    md_ctrs <- unique(md[["country_code"]]) # missing data countries
+
+  } else { # if yes_md == FALSE
+    md_ctrs    <- character() # missing data countries
+    md_off_reg <- character()
+    md_year    <- numeric()
+    grp_use    <- character()
+    md         <- character()
+  }
+
+  return(list(md         = md,
+              md_year    = md_year,
+              md_off_reg = md_off_reg,
+              grp_use    = grp_use)
+  )
 }
 
 
