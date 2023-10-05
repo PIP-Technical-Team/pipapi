50cea85266e8dc65990abf701fcfcaf58733bed0
tonyfujs <tony.fujs@gmail.com>
2023-10-05 16:52:08 +0200

    refactor code into self contained fucntions for easier testing and maintenance

--- R/create_countries_vctr.R
+++ R/create_countries_vctr.R
@@ -15,173 +15,84 @@ create_countries_vctr <- function(country,
                                   valid_years,
                                   aux_files) {
 
-  # init Return list ------------
+  # STEP 1: Setup ----
+  ## init Return list ----
   lret <-
     list(
       user_off_reg      = NULL, # Off regs requested by user
       user_alt_agg      = NULL, # Alt aggs requested by user
-      user_aggs         = NULL, # all aggregates requested by user
-      user_ctrs         = NULL, # countries requested by user
-      impl_ctrs         = NULL, # implicit countries requested by user
       est_ctrs          = NULL, # All countries needed for estimations
-      alt_agg           = NULL, # Alt aggregates available
-      off_reg           = NULL, # Official regions available
-      all_agg           = NULL, # All aggs available, including WLD and all
-      off_alt_agg       = NULL, # what to do with alt and reg
-      user_gt           = NULL, # gt implicitly selected by user
-      user_alt_gt       = NULL, # Alternative gt implicitly selected by user
-      ctr_alt_agg       = NULL, # ctrs in alt aggs based on gt by user
-      ctr_off_reg       = NULL, # Survey countries in official regions
-      md_ctrs           = NULL, # missing data countries
-      fg_ctrs           = NULL, # fill gaps countries (with svy)
-      md_off_reg        = NULL, # off regs to input to MD
-      md_year           = NULL, # years to input to MD countries
-      grp_use           = NULL,
+      fg_ctrs           = NULL, # All countries (with svy) needed for estimations when fill_gaps = TRUE
+      md_off_reg        = NULL, # off regs needed to input to missing data
+      md_year           = NULL, # years to input to missing data countries
+      grp_use           = NULL, # Instruction for the next function call
       md                = NULL, # missing data  and pop table
-      user_alt_gt_code  = NULL # code of alt grouping data
+      user_alt_gt_code  = NULL#,  # code of alt grouping data
+      # user_aggs         = NULL, # all aggregates requested by user
+      # user_ctrs         = NULL, # countries requested by user
+      # impl_ctrs         = NULL, # implicit countries requested by user
+      # alt_agg           = NULL, # Alt aggregates available
+      # off_reg           = NULL, # Official regions available
+      # all_agg           = NULL, # All aggs available, including WLD and ALL
+      # off_alt_agg       = NULL, # what to do with alt and reg
+      # user_gt           = NULL, # gt implicitly selected by user
+      # user_alt_gt       = NULL, # Alternative gt implicitly selected by user
+      # ctr_alt_agg       = NULL, # ctrs in alt aggs based on gt by user
+      # ctr_off_reg       = NULL, # Survey countries in official regions
+      # md_ctrs           = NULL  # missing data countries
     )
 
-  # modify if year is "ALL" --------------
-
+  ## Adjust year ----
   if (any(c("ALL", "MRV") %in% toupper(year))) {
     year <- valid_years$valid_survey_years
   }
-
-  #   ___________________________________________________________________
-  #   Organize main country and region data                       ####
-
-  ## Split between regions and countries ----------
-
-  ### Regions available ----------
+  # STEP 2: Identify regions ----
+  ## All available aggregates ----
   aggs      <- aux_files$regions  ## all aggregates
-
-  region_code <- country_code <-
-    grouping_type <- NULL
-
-  ## Official grouping type
-  # off_gt <-  c("region", "world")
-  off_gt <-  c("region")
-
-  # Official valid region codes
+  ## Official grouping type ----
+  off_gt <-  c("region") # c("region", "world")
+  ## Official valid region codes ----
   off_reg <- aggs[["region_code"]][aggs[["grouping_type"]] %in% off_gt]
-
-  #  Aggregates selected by user
-  if (any(c("ALL", "WLD") %in% country)) {
-
-    user_aggs <- off_reg
-
-  } else {
-
-    user_aggs <- unique(aggs[["region_code"]][aggs[["region_code"]] %in% country])
-  }
-
-  ## all and WLD to off_reg
-  off_reg <- unique(c("ALL", off_reg, "WLD"))
-
-  # Alternative  aggregates code
+  ## Extended list of regions ----
+  ext_reg <- c("ALL", "WLD")
+  off_reg_ext <- sort(unique(c(off_reg, ext_reg)))
+  ## Alternative  aggregates code ----
   alt_agg <- aggs[["region_code"]][!aggs[["grouping_type"]] %in% off_gt]
-
-  # All aggregates available including WLD and all
-  all_agg <- c(off_reg, alt_agg)
-
-  # Official aggregates requested by user
-  user_off_reg <- off_reg[off_reg %in% user_aggs]
-
-
-  # Alternative aggregates requested by user
+  ## All aggregates available including WLD and all ----
+  all_agg <- c(off_reg_ext, alt_agg)
+  ##  Aggregates selected by user ----
+  user_aggs <- select_user_aggs(country = country,
+                                off_reg = off_reg,
+                                aggs = aggs)
+  ## Official aggregates requested by user ----
+  user_off_reg <- off_reg_ext[off_reg_ext %in% user_aggs]
+  ## Alternative aggregates requested by user ----
   user_alt_agg <- alt_agg[alt_agg %in% user_aggs]
-
-
-  ### countries Available -----
-  ctrs      <- aux_files$countries
-
-  # Countries selected by user
-  user_ctrs <- unique(ctrs[["country_code"]][ctrs[["country_code"]]  %in% country])
-
-  ### Get grouping type -------
+  ## Get grouping type ----
   user_gt <- unique(aggs[["grouping_type"]][aggs[["region_code"]] %in% user_aggs])
+  ## Select off_alt_agg ----
+  off_alt_agg <- select_off_alt_agg(user_gt = user_gt, off_gt = off_gt)
 
-
-  if (!is_empty(user_gt) && all(user_gt %in% off_gt)) {
-    off_alt_agg <- "off"
-  } else if (any(off_gt %in% user_gt)) {
-    off_alt_agg <- "both"
-  } else {
-    off_alt_agg <- "alt"
-  }
-
-
-
-  ### Estimates for official aggregates
-
-  # Organize Countries in aggregate --------
-
-  #Find out all the countries that belong to
-  #ALTERNATIVE aggregates requested by the user
-
-  # Get countries that belong to aggregates requested by the user that are NOT
-  # official but alternative aggregates. We need to find out missing data
-  # estimates only for those countries. For instance, if the user requested LAC
-  # and AFE, we don't care about the the countries with missing data in the LAC
-  # because their estimates are done implicitly. We DO care about the estimates
-  # of the missing countries in AFE because we need the explicit SSA estimates.
-
-  cl           <- aux_files$country_list
-
-  if (!is_empty(user_gt)) {
-    user_alt_gt  <- user_gt[!user_gt %in% off_gt]
-    user_gt_code <- paste0(user_gt, "_code")
-  } else {
-    user_alt_gt  <- character()
-    user_gt_code <- character()
-  }
-
-  ## ALL Countries in alternative aggregates  ----
-  if (!is_empty(user_alt_gt)) {
-    user_alt_gt_code <- paste0(user_alt_gt, "_code")
-
-    ctr_alt_agg      <- user_alt_gt_code |>
-      # Create filter for data.table
-      paste0(" %in% user_alt_agg", collapse =  " | ") |>
-      # parse the filter as unevaluated expression
-      {\(.) parse(text = .) }() |>
-      # filter and get country codes
-      {\(.) cl[eval(.), country_code] }()
-
-  } else {
-    user_alt_gt_code <- character()
-    ctr_alt_agg      <- character()
-  }
-  # add to return list
-
-
-
-  ## ctr_off_reg Survey countries in official regions --------
-  if (!is_empty(user_off_reg)) {
-    ctr_off_reg <- ctrs[["country_code"]][ctrs[["region_code"]] %in% user_off_reg]
-  } else {
-    ctr_off_reg <- character()
-  }
-
-
-  ## Implicit SURVEY countries on both, official and alternative ----------
-  if (!is_empty(user_gt)) {
-    impl_ctrs   <- user_gt_code |>
-      # Create filter for data.table
-      paste0(" %in% user_aggs", collapse =  " | ") |>
-      # parse the filter as unevaluated expression
-      {\(.) parse(text = .) }() |>
-      # filter and get country codes
-      {\(.) ctrs[eval(.), country_code] }()
-
-  } else {
-    impl_ctrs <- character()
-  }
-
-  # add to return list
-
-
-  ## All countries needed for estimations --------------
+  # STEP 3: Identify countries ----
+  ## Available countries -----
+  ctrs      <- aux_files$countries
+  ## Countries selected by user ----
+  user_ctrs <- unique(ctrs[["country_code"]][ctrs[["country_code"]]  %in% country])
+  ## Countries need to compute alternative aggregates ----
+  user_alt_gt      <- get_user_alt_gt(user_gt)
+  user_gt_code     <- get_user_x_code(user_gt)
+  user_alt_gt_code <- get_user_x_code(user_alt_gt)
+  ctr_alt_agg      <- get_ctr_alt_agg(user_alt_gt      = user_alt_gt,
+                                      user_alt_gt_code = user_alt_gt_code,
+                                      cl               = aux_files$country_list )
+  ## Survey countries in official regions -----
+  ctr_off_reg <- get_ctr_off_reg(user_off_reg = user_off_reg,
+                                 ctrs = ctrs)
+  ## Implicit SURVEY countries in both, official and alternative ----
+  impl_ctrs <- get_impl_ctrs(user_gt = user_gt,
+                             user_gt_code = user_gt_code,
+                             ctrs = ctrs)
+  ## All countries needed for estimations -----
   est_ctrs <- unique(c(user_ctrs, impl_ctrs))
 
 
@@ -286,3 +197,159 @@ filter_md <- function(md, ctr_alt_agg, year) {
   }
   return(md)
 }
+
+#' Helper function to identify how Official and Alternative regions should be
+#' handled
+#'
+#' @param user_gt character: Grouping type implicitly selected by user
+#' @param off_gt character: Grouping type associated with Offical Regions
+#'
+#' @return character
+#' @export
+#'
+select_off_alt_agg <- function(user_gt, off_gt) {
+  if (!is_empty(user_gt) && all(user_gt %in% off_gt)) {
+    out <- "off"
+  } else if (any(off_gt %in% user_gt)) {
+    out <- "both"
+  } else {
+    out <- "alt"
+  }
+  return(out)
+}
+
+#' Helper function to select correct Official Regions
+#'
+#' @param country character: User selected countries
+#' @param off_reg character: Official region codes
+#' @param aggs data.frame: Regions lookup table
+#'
+#' @return character
+#' @export
+#'
+select_user_aggs <- function(country, off_reg, aggs) {
+  if (any(c("ALL", "WLD") %in% country)) {
+    # Select all official regions
+    out <- off_reg
+
+  } else {
+    # Select only official regions that correspond to selected countries
+    out <- unique(aggs[["region_code"]][aggs[["region_code"]] %in% country])
+  }
+  return(out)
+}
+
+#' Helper function to define user_alt_gt
+#'
+#' @param user_gt character: Grouping type needed by user
+#'
+#' @return character
+#' @export
+#'
+get_user_alt_gt <- function(user_gt) {
+  if (!is_empty(user_gt)) {
+    out  <- user_gt[!user_gt %in% off_gt]
+  } else {
+    out  <- character()
+  }
+  return(out)
+}
+
+#' Helper function to define user_{var}_code
+#'
+#' @param user_gt character: Grouping type needed by user
+#'
+#' @return character
+#' @export
+#'
+get_user_x_code <- function(x) {
+  if (!is_empty(x)) {
+    out <- paste0(x, "_code")
+  } else {
+    out <- character()
+  }
+  return(out)
+}
+
+#' Helper function to retrieve the required countries
+#' needed to compute alternative aggregates requested by user
+#' Get countries that belong to aggregates requested by the user that are NOT
+#' official but alternative aggregates. We need to find out missing data
+#' estimates only for those countries. For instance, if the user requested LAC
+#' and AFE, we don't care about the the countries with missing data in the LAC
+#' because their estimates are done implicitly. We DO care about the estimates
+#' of the missing countries in AFE because we need the explicit SSA estimates.
+#'
+#' @param user_alt_gt character: Grouping type needed by user
+#' @param user_alt_gt_code character: Grouping type code
+#' @param cl data.frame: Countries lookup table
+#'
+#' @return character
+#' @export
+#'
+get_ctr_alt_agg <- function(user_alt_gt,
+                            user_alt_gt_code,
+                            cl) {
+  if (!is_empty(user_alt_gt)) {
+
+    out      <- user_alt_gt_code |>
+      # Create filter for data.table
+      paste0(" %in% user_alt_agg", collapse =  " | ") |>
+      # parse the filter as unevaluated expression
+      {\(.) parse(text = .) }() |>
+      # filter and get country codes
+      {\(.) cl[eval(.), country_code] }()
+  } else {
+    out      <- character()
+  }
+  return(out)
+}
+
+#' Helper function to retrieve the required countries
+#' needed to compute official aggregates
+#'
+#' @param user_off_reg character: Official regions requested by user
+#' @param ctrs data.frame: Countries lookup table
+#'
+#' @return character
+#' @export
+#'
+get_ctr_off_reg <- function(user_off_reg,
+                            ctrs) {
+  if (!is_empty(user_off_reg)) {
+    out <- ctrs[["country_code"]][ctrs[["region_code"]] %in% user_off_reg]
+  } else {
+    out <- character()
+  }
+  return(out)
+}
+
+#' Helper function to retrieve the implicit country surveys present in both
+#' alternative and official aggregates
+#'
+#' @param user_gt character: Grouping type
+#' @param user_gt_code character: Grouping type code
+#' @param ctrs data.frame: Countries lookup table
+#'
+#' @return character
+#' @export
+#'
+get_impl_ctrs <- function(user_gt,
+                          user_gt_code,
+                          ctrs) {
+  if (!is_empty(user_gt)) {
+    out   <- user_gt_code |>
+      # Create filter for data.table
+      paste0(" %in% user_aggs", collapse =  " | ") |>
+      # parse the filter as unevaluated expression
+      {\(.) parse(text = .) }() |>
+      # filter and get country codes
+      {\(.) ctrs[eval(.), country_code] }()
+
+  } else {
+    out <- character()
+  }
+  return(out)
+}
+
+
