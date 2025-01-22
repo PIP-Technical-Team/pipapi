9dccdd70f6b2489b5a6dc150e398d6ebb21258dd
giorgiacek <giorgivola@gmail.com>
2025-01-22 05:09:29 -0500

    final instructions for local set-up with Ronak's modifications

--- inst/TMP/TMP_local_setup.R
+++ inst/TMP/TMP_local_setup.R
@@ -1,14 +1,23 @@
 # Set-up to test locally within a given branch
 
-# 1. Install the package in given branch if you need to, then hashtag and run as source.
-#library(devtools)
-#devtools::install_github("PIP-Technical-Team/pipapi@no-store")
+# 1. Install the package in given branch ----
+library(devtools)
+devtools::install_github("PIP-Technical-Team/pipapi@no-store") # this is for the given branch @no-store
 
-# 2. Load the packages
+# 2. Load the packages ----
 library(pipapi)
 library(fastverse)
 
-# 3. Prepare lkup
+# 3. Set Env Variables ----
+## Note: (This won't run unless you are in the Remote Computer)
+Sys.setenv(
+  PIP_ROOT_DIR = "//wbgmsddg001/pip/pipapi_data",
+  PIPAPI_DATA_ROOT_FOLDER_LOCAL =fs::path("e:/PIP/pipapi_data/"),
+  PIPAPI_APPLY_CACHING = FALSE
+)
+
+# 4. Prepare lkup(s) ----
+## Note: we set to latest release ----
 force <- FALSE
 if (!"lkups" %in% ls() || isTRUE(force)) {
   data_dir <- Sys.getenv("PIPAPI_DATA_ROOT_FOLDER_LOCAL") |>
@@ -26,19 +35,7 @@ lkups <- pipapi:::create_versioned_lkups(data_dir,
 lkup <- lkups$versions_paths[[lkups$latest_release]]
 
 # 4. Start the api using start_api()
+pipapi:::start_api(api_version = "v1", port = 8080)
 
-api_version <- "v1"
-version_path <- sprintf(
-  "plumber/%s/plumber.R",
-  api_version
-)
-api_path <- system.file(version_path, package = "pipapi")
-api <- source(api_path)
-
-host <- "0.0.0.0"
-port <- 8080
-plumber::pr_run(api$value, host = host, port = port)
-
-#pipapi:::start_api(api_version = "v1", port = 8080)
-
-
+# 5. Move to postman to test API itself.
+# 6. Move to pipr to test specific features.
