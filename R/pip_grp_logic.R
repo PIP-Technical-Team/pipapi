c1d6704281cc74ee2fd181921edf9724c49c1c6f
Ronak Sunil Shah <rshah11@worldbank.org>
2025-03-25 13:29:34 -0400

    fix pip group

--- R/pip_grp_logic.R
+++ R/pip_grp_logic.R
@@ -83,9 +83,13 @@ pip_grp_logic <- function(country         = "ALL",
       ppp             = NULL,
       lkup            = lkup
       )
-
     # For now just rowbinding two dataframes, but we would need to use it more smartly in the future
     fg_pip_master <- collapse::rowbind(fg_pip_master)
+
+    if (!data.table::is.data.table(fg_pip_master)) {
+      setDT(fg_pip_master)
+    }
+
     add_vars_out_of_pipeline(fg_pip_master, fill_gaps = TRUE, lkup = lkup)
 
     if (lcv$off_alt_agg == "both") {
