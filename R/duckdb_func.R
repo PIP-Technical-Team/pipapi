5ac9b8dc949a6d0fe9239407152c70d0331210d0
shahronak47 <shahronak47@yahoo.in>
2025-05-14 23:36:03 +0530

    set read only as FALSE

--- R/duckdb_func.R
+++ R/duckdb_func.R
@@ -124,7 +124,7 @@ reset_cache <- function(pass = Sys.getenv('PIP_CACHE_LOCAL_KEY'), type = c("both
 }
 
 create_duckdb_file <- function(cache_file_path) {
-  con <- connect_with_retry(cache_file_path)
+  con <- connect_with_retry(cache_file_path, read_only = FALSE)
   DBI::dbExecute(con, "CREATE OR REPLACE table rg_master_file (
                  country_code VARCHAR,
                  survey_id VARCHAR,
