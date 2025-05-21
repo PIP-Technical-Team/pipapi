6d77848dd79872379070eddd6aee685279b84b72
R.Andres Castaneda <acastastanedaa@worldbank.org>
2025-05-20 21:05:50 -0400

    solve big issue

--- R/pip.R
+++ R/pip.R
@@ -144,14 +144,10 @@ pip <- function(country         = "ALL",
 
     if (nrow(main_data) > 0) {
       out <- main_data |>
-        # collapse::fmutate(path = as.character(path)) |>
         rowbind(cached_data)
-      # cached_data is NULL when we are querying live data in which case we don't update cache
-      # This will be used only for development purpose and we don't have any intention to use it in production.
-      if(!is.null(cached_data)) {
-        # Update cache with data
+
         update_master_file(main_data, cache_file_path, fill_gaps)
-      }
+
     } else {
       out <- cached_data
     }
