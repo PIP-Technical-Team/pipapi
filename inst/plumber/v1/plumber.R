91a8adfcb8bd197e72e14239bae356d3a861a291
shahronak47 <shahronak47@yahoo.in>
2025-07-16 22:22:19 +0530

    add logs

--- inst/plumber/v1/plumber.R
+++ inst/plumber/v1/plumber.R
@@ -2,31 +2,26 @@ library(plumber)
 
 endpoints_path <- system.file("plumber/v1/endpoints.R", package = "pipapi")
 api_spec_path <-  system.file("plumber/v1/openapi.yaml", package = "pipapi")
-# convert_empty <- pipapi:::convert_empty
 
 plumber::pr(endpoints_path) |>
   # pre-route log
-  plumber::pr_hook("preroute", function() {
-    # log_separator()
-    # tictoc::tic("route") # Start timer for log info
+  plumber::pr_hook("preroute", function(req) {
   }) |>
   # post-route log
   plumber::pr_hook("postroute", function(req, res) {
-    # end_route <- tictoc::toc(quiet = TRUE)
-    # log_info('route: {convert_empty(req$REMOTE_ADDR)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(req$QUERY_STRING)}  {convert_empty(res$status)} {round(end_route$toc - end_route$tic, digits = getOption("digits", 6))}')
   }) |>
   # pre-serialization log
   plumber::pr_hook("preserialize", function() {
-    # tictoc::tic("serialize")
   }) |>
   # post-serialization log
   plumber::pr_hook("postserialize", function(req) {
-    # end_serial <- tictoc::toc(quiet = TRUE)
-    # log_info('serialize: {convert_empty(req$PATH_INFO)} {round(end_serial$toc - end_serial$tic, digits = getOption("digits", 6))}')
-    # log_separator()
   }) |>
   plumber::pr_hook("exit", function() {
-    # log_info('Bye bye: {proc.time()[["elapsed"]]}')
+  }) |>
+  plumber::pr_set_error(function(req, res, err) {
+    method <- req$REQUEST_METHOD
+    path <- req$PATH_INFO
+    cat(sprintf("ERROR at %s %s: %s\n", method, path, err$message))
   }) |>
   # Set API spec
   plumber::pr_set_api_spec(api = function(spec) {
